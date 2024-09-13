ffdb1fce924b8530a49bae61cc8fd708e1553afc
naren-loganathan <naren.spirion@gmail.com>
2024-09-13 14:25:26 -0400

    Add department tests/fix bugs

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
@@ -2,7 +2,8 @@ package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,6 +14,32 @@ import org.springframework.test.context.ContextConfiguration;
 @SpringBootTest
 @ContextConfiguration
 public class DepartmentUnitTests {
+  @BeforeEach
+  public void setupDepartmentForTesting() {
+    Course testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
+    HashMap<String, Course> courseHashMap = new HashMap<>();
+    courseHashMap.put("Test ID", testCourse);
+    testDepartment = new Department("Test Code", courseHashMap, "Test Chair", 0);
+  }
+
+  @Test
+  public void toStringTest() {
+    String expectedResult =
+        "Test Code Test ID: \nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55\n";
+    assertEquals(expectedResult, testDepartment.toString());
+  }
+
+  @Test
+  public void addAndDropPersonFromMajorTest() {
+    assertEquals(0, testDepartment.getNumberOfMajors());
+    testDepartment.addPersonToMajor();
+    assertEquals(1, testDepartment.getNumberOfMajors());
+    testDepartment.dropPersonFromMajor();
+    assertEquals(0, testDepartment.getNumberOfMajors());
+    testDepartment.dropPersonFromMajor();
+    assertEquals(0, testDepartment.getNumberOfMajors());
+  }
+
   public static Department testDepartment;
 }
 

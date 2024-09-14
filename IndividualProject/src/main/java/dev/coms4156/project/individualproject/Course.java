79f84a5a19489c7e71bf443e3ca7663fc6dc84df
naren-loganathan <naren.spirion@gmail.com>
2024-09-13 21:08:21 -0400

    Fix PMD issues

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -67,7 +67,7 @@ public class Course implements Serializable {
     return this.courseTimeSlot;
   }
 
-
+  @Override
   public String toString() {
     return "\nInstructor: " + instructorName + "; Location: " + courseLocation + "; Time: "
         + courseTimeSlot;
